4bc28e997a1460c41af8421c96e931ade342286a
sakkke <w32w64@gmail.com>
2023-08-19 18:43:50 +0900

    :ambulance: fix env and embed it in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/sh -l
 
 # Need hardcoding for GitHub Actions
-. /home/build/.cargo/env
+
+# rustup shell setup
+# affix colons on either side of $PATH to simplify matching
+case ":${PATH}:" in
+  *:"/home/build/.cargo/bin":*)
+    ;;
+  *)
+    # Prepending path in case a system-installed rustc needs to be overridden
+    export PATH="/home/build/.cargo/bin:$PATH"
+    ;;
+esac
 
 exec /entrypoint.nu
