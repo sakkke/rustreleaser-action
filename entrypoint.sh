7cdf013073a82d3ddc80509eba013a34430f1b6f
sakkke <w32w64@gmail.com>
2023-08-20 15:30:19 +0900

    :ambulance: fix mount source

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
 exec docker run \
-  --mount type=bind,src="$GITHUB_WORKSPACE",dst=/work \
+  --mount type=bind,src="$GH_WORKSPACE",dst=/work \
   --rm \
   ghcr.io/sakkke/rustreleaser:main
