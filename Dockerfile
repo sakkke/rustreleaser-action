a7e31a07bca2a8d2b4babdb615b588c1608bb365
sakkke <w32w64@gmail.com>
2023-08-21 23:23:12 +0900

    :ambulance: simplify Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,1 @@
-FROM alpine:3.10
-
-RUN apk update && apk add --no-cache docker-cli
-
-COPY entrypoint.sh /entrypoint.sh
-
-ENTRYPOINT [ "/entrypoint.sh" ]
+FROM ghcr.io/sakkke/rustreleaser:main
--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -l
-
-repository="$(basename "$GITHUB_REPOSITORY")"
-src="/home/runner/work/$repository/$repository"
-
-exec docker run \
-  --mount type=bind,src="$src",dst=/work \
-  --rm \
-  ghcr.io/sakkke/rustreleaser:main
